fc1851d72d6998408d5765ae75772acd9cd136e7
Guillermo C <gcifuentes@escanda.cc>
2022-06-29 12:12:22 +0200

    More work in mediator

--- crates/mediator/src/main.rs
+++ crates/mediator/src/main.rs
@@ -1,7 +1,35 @@
+use std::collections::HashMap;
+use clap::Parser;
+use clap::Subcommand;
+
 pub mod ipc_proto {
     tonic::include_proto!("ipc_proto");
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    #[clap(short = 'K', long = "kafka-host")]
+    pub kafka_host: String,
+
+    #[clap(short = 'k', long = "kafka-port")]
+    pub kafka_port: i16,
+
+    #[clap(short = 'S', long = "schema-host")]
+    pub schema_host: String,
+
+    #[clap(short = 's', long = "schema-port")]
+    pub schema_port: i16,
+
+    #[clap(short = 'F', long = "fn-host")]
+    pub fn_host: String,
+
+    #[clap(short = 'f', long = "fn-port")]
+    pub fn_port: i16,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    Ok(())
 }
